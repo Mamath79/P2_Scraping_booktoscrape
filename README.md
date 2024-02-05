773e802ce4999a63e4679e4b211448feea060e68
Mathieu Vieillefont <mathieu.vieillefont@gmail.com>
2024-02-05 16:41:39 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# 1. Description générale du projet
+
+Dans le cadre de la formation Python d'openclassrooms,j'ai écrit ce programme de scraping afin d'ectraire toutes les données du site https://books.toscrape.com/.
+le programme Scraping_book.toscrape_all.py prenant une vingtaine de minutes afin d'être entièrement executé, le programme Scraping_book.toscrape_sample.py est présent afin de montrer lors de la soutenance le fonctionnement sur un nombre reduit de catégories.
+
+# 2. Installation du programme
+
+Pré-requis: Avoir Python d'installer sinon voici le lien de telechargement de python 3 https://www.python.org/downloads/
+
+Clonez le dossier stocké sur Github via la commande : git clone https://github.com/Mamath79/Openclassrooms_formation_Python_Project_P2_Scraping_booktoscrape.git
+
+Placez vous dans le bon dossier avec la commande: cd Openclassrooms_formation_Python_Project_P2_Scraping_booktoscrape/
+
+Créez l'environnement de travail virtuel avec la commande : ```python3 -m venv scraping-venv```
+
+Activez l'environnement virtuel avec la commande :source scraping-venv/bin/Activate sous unix et mac ou scraping-venv\Scripts\Activate sous windows
+
+Installez les packages nécessaires listés dans le fichier requirements.txt via la commande: pip install -r requirements.txt
+
+le programme est prêt à être executer
+
+# 3. Fonctionnalités
+
+Le programme va créer un dossier Books to Scrape_Datas, dans lequel il va créer un sous dossier par catégorie contenant un fichier csv avec toutes les données extraites pour chaque livre de la categorie , ainsi qu'un dossier contenant toutes les couvertures des livres de la catégorie
+
+# 4. Démarrage du programme
+
+Lancez Scraping_book.toscrape_all.py
+
+
+
+
+
+
+
+
---
+++ Scraping_booktoscrape_sample.py
@@ -0,0 +1,100 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+import shutil
+import os.path
+
+# fonction 1: request and soup() ; request et parse d'une url
+def request_and_soup(x):
+    reponse = requests.get(x)
+    HTML = reponse.content
+    soup  = BeautifulSoup(HTML, "html.parser")
+    return soup
+
+# fonction 2: url_list_category_all_pages(); creation d'une liste de toutes les pages d'une catgorie (pouvant être superieur à 7, ex:default)
+def url_list_category_all_pages(z):
+    url_list_category_x=[]
+    for i in range(1,13):
+        url_list_category_x.append(z.replace("index",  ("page-" + str(i))))
+    a = url_list_category_x[0]
+    b = a.replace("page-1", "index")
+    url_list_category_x[0] = b
+    return url_list_category_x
+
+# fonction 3: extract_books_link ;creation de la liste de liens des livres par page de categorie
+def extract_books_link(w):
+    soup = request_and_soup(w)
+    category_book_link_page_x =[]
+    href_a_tag = soup.find_all('h3')
+    for h3 in href_a_tag:
+      category_book_link_page_x.append(h3.a['href'])
+    category_book_link_page_x = [s.replace("../../..", "https://books.toscrape.com/catalogue") for s in category_book_link_page_x]
+    return category_book_link_page_x
+
+# fonction 4: extract_book_data; recuperation data pour chaque liens de la category 
+def extract_book_data(y):
+    soup = request_and_soup(y)
+    book_title = soup.h1.string
+    description  = soup.find("meta", attrs={"name": "description"}).get("content").lstrip()
+    book_img_link = soup.find("img").get('src').replace("../..", "https://books.toscrape.com")
+    star_rating = soup.find("p",class_="star_rating")
+    informations = [y, book_title, description, book_img_link, star_rating]
+    for infos in soup.find_all('td'):
+        informations.append(infos.string)
+    a = informations[9]
+    b = a.split()
+    c = b[2]
+    d = c.strip("(")
+    informations[9]=d
+    return informations
+
+#fonction 5: category_book_data_csv_and_download_img ; creation du fichier csv , donwload ensemble des img de la category dans un dossier specific
+def category_book_data_csv_and_download_img(v):
+    properties = ["book_url", "book_title", "description" ,"img link","star_rating", "upc", "product type", "price (excl. tax) en £", "price (incl. tax) en £", "tax en £", "availabilily", "numbers of review"]
+    soup = request_and_soup(v)
+    category_title = soup.h1.text
+    a = f"{category_title}_dir"
+    b = f"{category_title}_img"
+    os.makedirs(str(a+"/"+b),exist_ok=True)   
+    with open(os.path.join(a, f"{category_title}_data.csv"), "w", newline="") as file:
+        writer = csv.writer(file, delimiter=",")
+        writer.writerow(properties)
+        for i in book_data:
+            writer.writerow(i)
+    for i in book_data:
+        book_title = i[1]
+        book_link_img = i[3]
+        try:
+            f = open(f"{category_title}.{book_title}.jpg", "wb")
+            response = requests.get(book_link_img)
+            f.write(response.content)
+            f.close()
+            shutil.move(f"{category_title}.{book_title}.jpg",str(a+"/"+b))
+        except : 
+            pass
+    try:
+        shutil.move(a,"Books to Scrape_Datas")
+    except:
+        pass
+
+URL = "https://books.toscrape.com/index.html"
+
+os.makedirs("Books to Scrape_Datas",exist_ok=True)
+page_links =[]
+soup = request_and_soup(URL)
+for a in soup.find_all('a', href=True):
+    page_links.append(a["href"])
+
+category_links = page_links[3:4]
+category_links = [s.replace("catalogue", "https://books.toscrape.com/catalogue") for s in category_links]
+
+
+for category_url in category_links:
+    url_list_category_x = url_list_category_all_pages(category_url)
+    category_book_link_all_imbriquee= list(map(extract_books_link, url_list_category_x))
+    category_book_link_all = sum(category_book_link_all_imbriquee,[])
+    book_data =[]
+    for l in category_book_link_all:
+        extract_book_data(l)
+        book_data.append(extract_book_data(l))
+    category_book_data_csv_and_download_img(category_url)
