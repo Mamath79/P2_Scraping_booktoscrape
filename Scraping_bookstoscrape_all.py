c563605cdea18ef7957d6781afd0f8cb36c614bf
Mathieu Vieillefont <mathieu.vieillefont@gmail.com>
2024-02-07 09:56:07 +0100

    inclusion du star rating dans l'extract datas

--- Scraping_bookstoscrape_all.py
+++ Scraping_bookstoscrape_all.py
@@ -14,7 +14,7 @@ def request_and_soup(x):
 # fonction 2: url_list_category_all_pages(); creation d'une liste de toutes les pages d'une catgorie (pouvant être superieur à 7, ex:default)
 def url_list_category_all_pages(z):
     url_list_category_x=[]
-    for i in range(1,13):
+    for i in range(1,10):
         url_list_category_x.append(z.replace("index",  ("page-" + str(i))))
     a = url_list_category_x[0]
     b = a.replace("page-1", "index")
@@ -37,19 +37,24 @@ def extract_book_data(y):
     book_title = soup.h1.string
     description  = soup.find("meta", attrs={"name": "description"}).get("content").lstrip()
     book_img_link = soup.find("img").get('src').replace("../..", "https://books.toscrape.com")
-    informations = [y, book_title, description, book_img_link]
+    star_rating_soup= soup.find_all("p",{"class": "star-rating"})
+    star_rating_txt = star_rating_soup[0]["class"][1]
+    dict = {"Zero": 0,"One": 1,"Two" : 2,"Three": 3,"Four" : 4,"Five":5}
+    star_rating = dict[star_rating_txt]
+    informations = [y, book_title, description, book_img_link, star_rating]
     for infos in soup.find_all('td'):
         informations.append(infos.string)
-    a = informations[9]
+    informations[7:10] = [i.strip('£') for i in informations[7:10]]
+    a = informations[10]
     b = a.split()
     c = b[2]
     d = c.lstrip("(")
-    informations[9]=d
+    informations[10]=d
     return informations
 
 #fonction 5: category_book_data_csv_and_download_img ; creation du fichier csv , donwload ensemble des img de la category dans un dossier specific
 def category_book_data_csv_and_download_img(v):
-    properties = ["book_url", "book_title", "description" ,"img link", "upc", "product type", "price (excl. tax)", "price (incl. tax)", "tax", "availabilily", "numbers of review"]
+    properties = ["book_url", "book_title", "description" ,"img link","star rating", "upc", "product type", "price (excl. tax)", "price (incl. tax)", "tax", "availabilily", "numbers of review"]
     soup = request_and_soup(v)
     category_title = soup.h1.text
     a = f"{category_title}_dir"
@@ -64,17 +69,25 @@ def category_book_data_csv_and_download_img(v):
         book_title = i[1]
         book_link_img = i[3]
         try:
-            f = open(f"{category_title}.{book_title}.jpg", "wb")
+            f = open(f"{category_title}_{book_title}.jpg", "xb")
             response = requests.get(book_link_img)
             f.write(response.content)
             f.close()
-            shutil.move(f"{category_title}.{book_title}.jpg",str(a+"/"+b))
-        except : 
+        except:
             pass
-    try:
+        try:
+            shutil.move(f"{category_title}_{book_title}.jpg",str(a+"/"+b), copy_function= shutil.copy )
+        except:
+            pass    
+    path = ('Books to Scrape_Datas/'+ a)  
+    if os.path.isdir(path) == False:
         shutil.move(a,"Books to Scrape_Datas")
-    except:
-        pass
+    else:
+        shutil.rmtree(path)
+        shutil.move(a,"Books to Scrape_Datas")
+
+
+
 
 URL = "https://books.toscrape.com/index.html"
 
